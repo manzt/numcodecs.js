56d89ef600d48385fbb17fb965d590b6979231e2
Trevor Manz <trevor.j.manz@gmail.com>
2020-05-06 15:49:10 -0400

    Add forked c-blosc submodule with compile script

---
+++ src/blosc-wasm/compile.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+mkdir build
+cd build
+
+# compile blosc to llvm using emscripten
+emcmake cmake ../c-blosc
+cmake --build .
+
+cd ../
+
+mkdir target
+
+cd target
+cp ../c-blosc/examples/simple.c .
+
+emcc simple.c \
+  -s WASM=1 \
+  -s ALLOW_MEMORY_GROWTH=1 \
+  -s EXTRA_EXPORTED_RUNTIME_METHODS='["cwrap"]' \
+  -s TOTAL_MEMORY=33554432 \
+  -I ../c-blosc/blosc \
+  -lblosc \
+  -L ../build/blosc
