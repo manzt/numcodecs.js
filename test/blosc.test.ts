cb1ec7cf3b3bcf934ba9ea367b77a0f744efed7b
Trevor Manz <trevor.j.manz@gmail.com>
2020-05-13 19:14:23 -0400

    tests still not working

--- test/blosc.test.ts
+++ test/blosc.test.ts
@@ -1,26 +1,25 @@
 import Blosc, {
-  Shuffle,
   BloscConfig,
-  CompressionLevel,
-  CompressionName,
+  BloscCompressionLevel,
+  BloscCompressor,
 } from '../src/blosc';
 
 import { range, linspace, product, checkAsyncEncodeDecode } from './common';
 
 const codecConfigs: BloscConfig[] = [
-  { shuffle: Shuffle.SHUFFLE },
-  { clevel: 0, shuffle: Shuffle.SHUFFLE },
-  { cname: 'lz4', shuffle: Shuffle.SHUFFLE },
-  { cname: 'lz4', clevel: 1, shuffle: Shuffle.NOSHUFFLE },
-  { cname: 'lz4', clevel: 5, shuffle: Shuffle.SHUFFLE },
-  { cname: 'lz4', clevel: 9, shuffle: Shuffle.BITSHUFFLE },
+  { shuffle: Blosc.SHUFFLE },
+  { clevel: 0, shuffle: Blosc.SHUFFLE },
+  { cname: 'lz4', shuffle: Blosc.SHUFFLE },
+  { cname: 'lz4', clevel: 1, shuffle: Blosc.NOSHUFFLE },
+  { cname: 'lz4', clevel: 5, shuffle: Blosc.SHUFFLE },
+  { cname: 'lz4', clevel: 9, shuffle: Blosc.BITSHUFFLE },
   { cname: 'zlib', clevel: 1, shuffle: 0 },
   { cname: 'zstd', clevel: 1, shuffle: 1 },
   { cname: 'blosclz', clevel: 1, shuffle: 2 },
   { cname: 'snappy', clevel: 1, shuffle: 2 },
-  { shuffle: Shuffle.SHUFFLE, blocksize: 0 },
-  { shuffle: Shuffle.SHUFFLE, blocksize: 2 ** 8 },
-  { cname: 'lz4', clevel: 1, shuffle: Shuffle.NOSHUFFLE, blocksize: 2 ** 8 },
+  { shuffle: Blosc.SHUFFLE, blocksize: 0 },
+  { shuffle: Blosc.SHUFFLE, blocksize: 2 ** 8 },
+  { cname: 'lz4', clevel: 1, shuffle: Blosc.NOSHUFFLE, blocksize: 2 ** 8 },
 ];
 
 const arrays = [
@@ -37,8 +36,8 @@ test('ensure all equal', async () => {
   )) {
     // Rather than reloading all the webassembly,
     // just reuse the loaded emscripten module and update the properties
-    if (cname) codec.cname = cname as CompressionName;
-    if (clevel) codec.clevel = clevel as CompressionLevel;
+    if (cname) codec.cname = cname as BloscCompressor;
+    if (clevel) codec.clevel = clevel as BloscCompressionLevel;
     if (shuffle) codec.shuffle = shuffle;
     if (blocksize) codec.blocksize = blocksize;
 
