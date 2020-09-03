d95f373ed4ea1c2ec916065c30d82e02b5b1f942
Trevor Manz <trevor.j.manz@gmail.com>
2020-09-03 13:47:13 -0400

    End to end works

--- codecs/lz4/lz4_codec.cpp
+++ codecs/lz4/lz4_codec.cpp
@@ -5,69 +5,37 @@
 
 using namespace emscripten;
 
-// https://github.com/zarr-developers/numcodecs/blob/master/numcodecs/stdint_compat.h
-static const int UINT32_SIZE = sizeof (uint32_t);
-
-static inline int load_le32(const char *c)
-{
-    const uint8_t *d = (const uint8_t *) c;
-    uint32_t x = d[0] | (d[1] << 8) | (d[2] << 16) | (d[3] << 24);
-    return (int) x;
-}
-
-static inline void store_le32(char *c, int y)
-{
-    uint32_t x = (uint32_t) y;
-    c[0] = x & 0xff;
-    c[1] = (x >> 8) & 0xff;
-    c[2] = (x >> 16) & 0xff;
-    c[3] = (x >> 24) & 0xff;
-}
-
 char *dest_ptr;
 val compress(std::string source, int acceleration)
 {
-    int dest_size, compressed_size;
-    char *dest_start;
+    const char *source_ptr = source.c_str();
+    const int source_size = source.size();
+    const int max_dest_size = LZ4_compressBound(source_size);
 
-    // setup destination
-    // NB: mostly translated from numcodecs/lz4.pyx
-    dest_size = LZ4_compressBound((int) source.size());
-    dest_ptr = (char *)malloc(dest_size + UINT32_SIZE);
-    store_le32(dest_ptr, source.size());
-    dest_start = dest_ptr + UINT32_SIZE;
+    dest_ptr = (char *)malloc((size_t)max_dest_size);
 
-    compressed_size = LZ4_compress_fast(
-        (const char *)source.c_str(),
-        dest_start,
-        (int) source.size(),
-        dest_size,
+    const int compressed_size = LZ4_compress_fast(
+        source_ptr,
+        dest_ptr,
+        source_size,
+        max_dest_size,
         acceleration
     );
-    compressed_size += UINT32_SIZE;
-    return val(typed_memory_view(compressed_size, (const uint8_t *)dest_ptr));
+
+    return val(typed_memory_view(compressed_size, (uint8_t *)dest_ptr));
 }
 
 val decompress(std::string source)
 {
-    const char *source_ptr;
-    const char *source_start;
-    int source_size, dest_size, decompressed_size;
-
     // setup source buffer
-    source_ptr = (const char *)source.c_str();
-    source_size = source.size();
-
-    // determine uuncompressed size
-    dest_size = load_le32(source_ptr);
-    source_start = source_ptr + UINT32_SIZE;
-    source_size -= UINT32_SIZE;
+    const char *source_ptr = source.c_str();
+    int source_size = source.size();
 
     // setup destination buffer
-    dest_ptr = (char *)malloc(dest_size);
+    dest_ptr = (char *)malloc((size_t)source_size);
 
-    decompressed_size = LZ4_decompress_safe(source_ptr, dest_ptr, source_size, dest_size);
-    return val(typed_memory_view(decompressed_size, (const uint8_t *)dest_ptr));
+    const int decompressed_size = LZ4_decompress_safe(source_ptr, dest_ptr, source_size, source_size);
+    return val(typed_memory_view(decompressed_size, (uint8_t *)dest_ptr));
 }
 
 void free_result()
--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 
-const codecs = ['zlib', 'gzip', 'blosc'];
+const codecs = ['zlib', 'gzip', 'blosc', 'lz4'];
 const inputs = Object.fromEntries(codecs.map((c) => [c, `./src/${c}.ts`]));
 
 export default [
