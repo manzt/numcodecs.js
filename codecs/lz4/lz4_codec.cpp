8695b8ff173088290bec3ef6da7d14a268877ee2
Trevor Manz <trevor.j.manz@gmail.com>
2020-12-23 10:05:43 -0500

    copy stdint_compat from numcodecs

--- codecs/lz4/lz4_codec.cpp
+++ codecs/lz4/lz4_codec.cpp
@@ -5,23 +5,44 @@
 
 using namespace emscripten;
 
+static inline void store_le32(char *c, int y)
+{
+    uint32_t x = (uint32_t) y;
+    c[0] = x & 0xff;
+    c[1] = (x >> 8) & 0xff;
+    c[2] = (x >> 16) & 0xff;
+    c[3] = (x >> 24) & 0xff;
+}
+
+
+static inline int load_le32(const char *c)
+{
+    const uint8_t *d = (const uint8_t *) c;
+    uint32_t x = d[0] | (d[1] << 8) | (d[2] << 16) | (d[3] << 24);
+    return (int) x;
+}
+
+static const int UINT32_SIZE = sizeof (uint32_t);
+
 char *dest_ptr;
 val compress(std::string source, int acceleration)
 {
     const char *source_ptr = source.c_str();
-    const int source_size = source.size();
-    const int max_dest_size = LZ4_compressBound(source_size);
+    int source_size = source.size();
+    int dest_size = LZ4_compressBound(source_size);
 
-    dest_ptr = (char *)malloc((size_t)max_dest_size);
+    dest_ptr = (char *)malloc((size_t)dest_size + UINT32_SIZE);
+    store_le32(dest_ptr, source_size);
+    char *dest_start = dest_ptr + UINT32_SIZE;
 
-    const int compressed_size = LZ4_compress_fast(
+    int compressed_size = LZ4_compress_fast(
         source_ptr,
-        dest_ptr,
+        dest_start,
         source_size,
-        max_dest_size,
+        dest_size,
         acceleration
     );
-
+    compressed_size += UINT32_SIZE;
     return val(typed_memory_view(compressed_size, (uint8_t *)dest_ptr));
 }
 
@@ -33,8 +54,11 @@ val decompress(std::string source)
 
     // setup destination buffer
     dest_ptr = (char *)malloc((size_t)source_size);
+    int dest_size = load_le32(source_ptr);
+    const char *source_start = source_ptr + UINT32_SIZE;
+    source_size -= UINT32_SIZE;
 
-    const int decompressed_size = LZ4_decompress_safe(source_ptr, dest_ptr, source_size, source_size);
+    const int decompressed_size = LZ4_decompress_safe(source_start, dest_ptr, source_size, dest_size);
     return val(typed_memory_view(decompressed_size, (uint8_t *)dest_ptr));
 }
 
