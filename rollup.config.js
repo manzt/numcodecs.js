71b2024b3c15688ac6a0ed8e6f2d5593d04687b4
Trevor Manz <trevor.j.manz@gmail.com>
2021-06-28 12:53:05 -0400

    add extension

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import resolve from '@rollup/plugin-node-resolve';
 import esbuild from 'rollup-plugin-esbuild';
-import base64 from './lib/plugin-base64';
+
+import base64 from './lib/plugin-base64.js';
 
 const codecs = ['zlib', 'gzip', 'blosc', 'lz4', 'zstd'];
 
