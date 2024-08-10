cdbe1d95399be9ede29f93335d993441580093e9
Mark Kittisopikul <kittisopikulm@janelia.hhmi.org>
2024-08-09 22:51:58 -0400

    Code clean up

--- codecs/zstd/zstd_codec.cpp
+++ codecs/zstd/zstd_codec.cpp
@@ -2,9 +2,6 @@
 #include "emscripten/val.h"
 #include "zstd.h"
 #include <string>
-#include <stdexcept>
-//#include <iostream>
-#include <algorithm>
 
 using namespace emscripten;
 
@@ -48,21 +45,21 @@ val decompress(std::string source)
     // setup destination buffer
     unsigned long long dest_size = ZSTD_getFrameContentSize(source_ptr, source_size);
 
+    // If Zstd_compressStream was used, we may not know the frame content size.
+    // https://github.com/manzt/numcodecs.js/issues/46
     if (dest_size == ZSTD_CONTENTSIZE_UNKNOWN) {
-        // guess
+        // guess decompressed buffer size based on source size
         dest_size = source_size*2;
 
         // Initialize the destination size to 1 MiB at minimum
         if (dest_size < 1024*1024)
             dest_size = 1024*1024;
     } else if (dest_size == ZSTD_CONTENTSIZE_ERROR) {
-        // error
         throw std::runtime_error("zstd codec error: content size error");
     } else if (dest_size < 0) {
         // unknown error
         throw std::runtime_error("zstd codec error: unknown ZSTD_getFrameContentSize error");
     }
-
 
     output.dst = malloc((size_t) dest_size);
 
@@ -73,30 +70,26 @@ val decompress(std::string source)
 
     output.size = dest_size;
 
+    // Call ZSTD_decompressStream repeatedly until status == 0 or error (status < 0)
     do {
         status = ZSTD_decompressStream(zds, &output, &input);
 
         if (ZSTD_isError(status)) {
-            // error
-            // TODO: free memory
             if (dest_ptr == output.dst)
                 dest_ptr = (char *) NULL;
             free(output.dst);
             throw std::runtime_error("zstd codec error: " + std::string(ZSTD_getErrorName(status)));
         }
 
-        // std::cerr << "Status: " << status << std::endl;
-        // std::cerr << "Size: " << output.size << std::endl;
-        // std::cerr << "Pos: " << output.pos << std::endl;
-
         if (status > 0 && output.pos == output.size ) {
-            // attempt to expand output buffer
-            size_t new_size = output.size + std::max(status, (size_t) 1024*1024);
+            // attempt to expand output buffer in 1 MiB (or more) increments
+            if (status < 1024*1024) {
+                status = (size_t) 1024*1024;
+            }
+            size_t new_size = output.size + status;
 
             if (new_size < output.size || new_size < status) {
                 // overflow error
-                if (dest_ptr == output.dst)
-                    dest_ptr = (char *) NULL;
                 free(output.dst);
                 throw std::runtime_error("zstd codec error: output buffer overflow");
             }
@@ -105,11 +98,11 @@ val decompress(std::string source)
             void *new_dst = realloc(output.dst, new_size);
 
             if (new_dst == NULL) {
-                if (dest_ptr == output.dst)
-                    dest_ptr = (char *) NULL;
+                // free the original pointer if realloc fails.
                 free(output.dst);
                 throw std::runtime_error("zstd codec error: could not expand output buffer");
             }
+            // the old output.dst is freed by realloc is it succeeds
             output.dst = new_dst;
 
             output.size = new_size;
